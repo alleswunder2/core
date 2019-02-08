fa5bbaa4f5521bb04505e50f940fa5654e015566
Dmitry Fedyuk <dfediuk@gmail.com>
2019-02-08 18:26:25 +0700

    1.0.3: https://github.com/alleswunder2/issues/issues/20

---
+++ view/frontend/web/productItemActions.js
@@ -0,0 +1,8 @@
+// 2019-02-08
+// "Remove white gaps from product lists in the mobile mode"
+// https://github.com/alleswunder2/issues/issues/20
+// https://www.upwork.com/ab/f/contracts/21555660
+define([], function() {'use strict';
+return function(sb) {
+sb.itemHeight = function() {};
+return sb;};});
